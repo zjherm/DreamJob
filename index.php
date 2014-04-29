4cf0bfdb9210897e32e3e9a59fe647eab7c63564
zjherm <zackjherman@gmail.com>
2014-04-28 18:41:55 -0700

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Twine</title>
+  <meta name="description" content="Twine">
+  <meta name="author" content="Digital Telepathy">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
